b11bdae57b16edbd29a91a7ac18516c192aeaf9e
NikitaMalofeev <nikita.m.edu@gmail.com>
2024-11-14 02:11:18 +0500

    fix qr scanner ui

--- employee-app/employee-app/src/shared/ui/QRScannerOverlay.tsx
+++ employee-app/employee-app/src/shared/ui/QRScannerOverlay.tsx
@@ -2,19 +2,30 @@ import React from 'react';
 
 export const QRScannerOverlay: React.FC = () => {
     return (
-        <div className="fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center">
-            <div className="relative w-80 h-80">
+        <div className="fixed inset-0 flex items-center justify-center" style={{ zIndex: 1, }}>
+            <div className="relative w-80 h-80" style={{ zIndex: 2 }}>
                 <div
-                    className="absolute inset-0 bg-black bg-opacity-70"
+                    className="absolute inset-0"
                     style={{
-                        clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%, 0 25%, 25% 25%, 25% 75%, 75% 75%, 75% 25%, 0 25%)',
+                        clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%, 0 12%, 12% 12%, 12% 88%, 88% 88%, 88% 12%, 0 12%)',
+
                     }}
                 ></div>
                 <div className="absolute top-0 left-0 w-8 h-8 border-t-4 border-l-4 border-blue-500"></div>
                 <div className="absolute top-0 right-0 w-8 h-8 border-t-4 border-r-4 border-blue-500"></div>
                 <div className="absolute bottom-0 left-0 w-8 h-8 border-b-4 border-l-4 border-blue-500"></div>
                 <div className="absolute bottom-0 right-0 w-8 h-8 border-b-4 border-r-4 border-blue-500"></div>
+
+                <div
+                    className="absolute inset-12 bg-transparent"
+                    style={{
+                        zIndex: 2,
+                    }}
+                >
+                </div>
             </div>
         </div>
+
+
     );
 };
